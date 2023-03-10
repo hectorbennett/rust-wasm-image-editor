4a91c540c039bffce7255a92a456eb45ae93931c
Hector <hectorbennett@outlook.com>
2023-03-10 00:56:49 +0000

    small fix

--- src/context/tools/bucket_fill.tsx
+++ src/context/tools/bucket_fill.tsx
@@ -1,21 +1,17 @@
 import { PaintBucket } from "react-bootstrap-icons";
-import { Tool, ToolEventParams } from ".";
+import { Tool } from ".";
 
 export const bucket_fill: Tool = {
   name: "bucket_fill",
   label: "Bucket Fill",
   icon: PaintBucket,
   events: {
-    onClick: function ({
-      ctx,
-      canvasLeft,
-      canvasTop,
-      canvasWidth,
-      canvasHeight,
-    }: // project,
-    ToolEventParams) {
-      // ctx.fillStyle = project.activeColourAsString;
-      ctx.fillRect(canvasLeft, canvasTop, canvasWidth, canvasHeight);
-    },
+    // onClick: function ({
+    //   // ctx,
+    // }: // project,
+    // ToolEventParams) {
+    //   // ctx.fillStyle = project.activeColourAsString;
+    //   // ctx.fillRect(canvasLeft, canvasTop, canvasWidth, canvasHeight);
+    // },
   },
 };
