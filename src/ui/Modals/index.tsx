ffe4881fe2923630d3af0c597eb4d9e8533bee3d
Hector <hectorbennett@outlook.com>
2023-04-13 09:44:42 +0100

    bugfix

--- src/ui/Modals/index.tsx
+++ src/ui/Modals/index.tsx
@@ -1,9 +1,6 @@
-import CreateNewProject from "./CreateNewProject";
 import ResizeCanvas from "./ResizeCanvas";
 
 const modals = {
-  "file.new": CreateNewProject,
-  "file.export": CreateNewProject,
   "image.resize_canvas": ResizeCanvas,
 };
 
