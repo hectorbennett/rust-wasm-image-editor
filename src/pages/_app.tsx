bf35162635ceefeb4936bcfa9618548aedd26b4d
Hector <hectorbennett@outlook.com>
2023-04-27 13:26:59 +0100

    Add meta tag

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -13,8 +13,9 @@ export default function App({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <title>Mantine next example</title>
+        <title>Rust wasm image editor</title>
         <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+        <meta name="description" content="Image editor built in rust" />
         <link rel="shortcut icon" href="/favicon.svg" />
       </Head>
 
