7afea58e6effaadd4785bbce56b60c6cc8e60d99
Hector <hectorbennett@outlook.com>
2023-04-30 01:50:00 +0100

    comment

--- wasm/src/app/layer.rs
+++ wasm/src/app/layer.rs
@@ -162,11 +162,7 @@ impl Layer {
     }
 
     pub fn crop(&mut self, left: i32, top: i32, width: u32, height: u32) {
-        // create a new buffer
-        // let buffer = PixelBuffer::new(width, height);
-
-        // // shift every pixel by the difference
-        // let x_diff =
+        // self.buffer.crop(left, top, right, bottom);
     }
 }
 
